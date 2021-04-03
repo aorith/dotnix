e09a77115d916a6cf72a4e0b923b9e2ac31dd8cc
Manuel Sanchez Pinar <aomanu@gmail.com>
2021-04-04 00:21:47 +0200

    Update templates

--- templates/iso-trantor.nix
+++ templates/iso-trantor.nix
@@ -1,12 +1,16 @@
 { nixpkgs ? <nixpkgs>, system ? "x86_64-linux", config, pkgs, ... }: {
   imports = [
-    <nixpkgs/nixos/modules/installer/cd-dvd/installation-cd-graphical-gnome.nix>
+    <nixpkgs/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix>
     <nixpkgs/nixos/modules/installer/cd-dvd/channel.nix>
   ];
+
+  nixpkgs.config.allowUnfree = true;
+  environment.systemPackages = with pkgs; [ git vim mkpasswd ];
 
   time.timeZone = "Europe/Madrid";
   networking = {
     hostName = "trantor";
+    hostId = "e7919205";
     usePredictableInterfaceNames = true;
     interfaces = {
       enp6s0f0 = {
@@ -17,23 +21,20 @@
       };
     };
   };
-  users.extraUsers.root.password = "root";
-  users.extraUsers.root.openssh.authorizedKeys.keys = [
-    "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCx2MbdR3PwSOrBFAlv4F8W0gNCRUa23b+YmGgRuwFTb9BmspWZ6bqUFZhDmBBLZMIh+1iatg6ZOlZhFmbbzRMXO+X1AX79QuKeRLb7doIdPiLvSgYyydurQZNYvG9/IbfjwpxvAt9sce52ZIuHlea/NGvPqH1J7w5sm1OP/F4iGiAh46j+/ZYXlB7kvDxwwtOx+owLJIXwmPnTgytWzjdt3IbR8aXk4h/ZMlk2s2G9LMhKq+6p/vkgEcJL1eb7R5unk97A6tWDu6lPLagMEDX+ANudl2yaLzlMzyox2BcbLl95zaXrM0VkPynsW/biQufjXbs9HFjp1/h25XAHA2xWkpqEpWGUrWhjP42mt3B4Ai4ILgAppS0JkdU+Iudff662ird5cGgOCcZru7ckDPT7cltzEUz+U3QXRZJe+nd09+zBNfhtdagw3oeXPqilxv+oKG9dgl5yTOCksicpmo2vuqNR6y9jakkvTxqV+TudW3hkSejCtWBsijM1Kwr8Qp0= aomanu@tazenda"
-  ];
-  nixpkgs.config.allowUnfree = true;
-  boot.kernelPackages = pkgs.linuxPackages_latest;
-  boot.initrd.kernelModules =
-    [ "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
-  boot.kernelModules = [ "kvm-amd" ];
 
-  systemd.services.sshd.wantedBy = pkgs.lib.mkForce [ "multi-user.target" ];
+  boot = {
+    kernelPackages = pkgs.linuxPackages_latest;
+    initrd.kernelModules =
+      [ "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
+    kernelModules = [ "kvm-amd" ];
+  };
+
+  users.extraUsers.root.password = "root";
   services = {
     openssh = {
       enable = true;
       permitRootLogin = "yes";
     };
   };
-
-  environment.systemPackages = with pkgs; [ bash git vim ];
+  systemd.services.sshd.wantedBy = pkgs.lib.mkForce [ "multi-user.target" ];
 }
--- templates/template.nix
+++ templates/template.nix
@@ -1,23 +1,75 @@
-{ pkgs, ... }: {
-  users.groups = {
-    aorith = {
-      name = "aorith";
-      members = [ "aorith" ];
-      gid = 9999;
+{ config, pkgs, ... }:
+let
+  password = CHANGETHIS;
+  hostName = "trantor";
+  hostId = "e7919205";
+in {
+  imports = [ ./hardware-configuration.nix ];
+
+  boot = {
+    loader.systemd-boot.enable = true;
+    loader.efi.canTouchEfiVariables = true;
+    kernelPackages = pkgs.linuxPackages_latest;
+  };
+
+  environment.systemPackages = with pkgs; [ wget vim firefox git mkpasswd ];
+
+  time.timeZone = "Europe/Madrid";
+  networking = {
+    firewall.enable = false;
+    hostName = "${hostName}"; # Define your hostname.
+    hostId = "${hostId}";
+    useDHCP = false;
+    interfaces = {
+      enp6s0f0.useDHCP = true;
+      enp6s0f1.useDHCP = true;
+      enp8s0.useDHCP = true;
     };
   };
 
-  users.users = {
-    aorith = {
-      isNormalUser = true;
-      description = "Manuel";
-      uid = 9999;
-      group = "aorith";
-      home = "/home/aorith";
-      createHome = true;
-      extraGroups = [ "wheel" "networkmanager" ];
-      shell = pkgs.bash;
-      password = EDITTHIS;
+  i18n.defaultLocale = "en_US.UTF-8";
+  console = {
+    font = "Lat2-Terminus16";
+    keyMap = "es";
+  };
+
+  services.xserver = {
+    enable = true;
+    displayManager.gdm.enable = true;
+    desktopManager.gnome3.enable = true;
+    layout = "es";
+  };
+
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+  services.openssh.enable = true;
+  systemd.services.sshd.wantedBy = pkgs.lib.mkForce [ "multi-user.target" ];
+
+  users = {
+    mutableUsers = false;
+    groups = {
+      aorith = {
+        name = "aorith";
+        members = [ "aorith" ];
+        gid = 9999;
+      };
+    };
+    extraUsers.root.password = "${password}";
+    users = {
+      aorith = {
+        isNormalUser = true;
+        description = "Manuel";
+        uid = 9999;
+        group = "aorith";
+        home = "/home/aorith";
+        createHome = true;
+        extraGroups = [ "wheel" "networkmanager" ];
+        shell = pkgs.bash;
+        password = "${password}";
+      };
     };
   };
+
+  system.stateVersion = "20.09"; # Did you read the comment?
 }
+
